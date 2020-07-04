9f184e49fd4e34bd5ab728cfb14935d44197482b
Andrew Rowland <andrew.rowland@amazerealise.com>
2020-07-04 11:39:37 +0100

    Added mobile navigation.

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -4,9 +4,10 @@
       <div class="header__top">
         <div class="container">
           <ul class="header__social">
-            <li><a href="https://www.facebook.com/rebalancingyou/" title="Visit me on Facebook"><svg viewBox="0 0 512 512" preserveAspectRatio="xMidYMid meet"><path d="M211.9 197.4h-36.7v59.9h36.7V433.1h70.5V256.5h49.2l5.2-59.1h-54.4c0 0 0-22.1 0-33.7 0-13.9 2.8-19.5 16.3-19.5 10.9 0 38.2 0 38.2 0V82.9c0 0-40.2 0-48.8 0 -52.5 0-76.1 23.1-76.1 67.3C211.9 188.8 211.9 197.4 211.9 197.4z"></path></svg></a></li>
             <li><a href="https://www.instagram.com/rebalancingyou/" title="Visit me on Instagram"><svg viewBox="0 0 512 512" preserveAspectRatio="xMidYMid meet"><path d="M256 109.3c47.8 0 53.4 0.2 72.3 1 17.4 0.8 26.9 3.7 33.2 6.2 8.4 3.2 14.3 7.1 20.6 13.4 6.3 6.3 10.1 12.2 13.4 20.6 2.5 6.3 5.4 15.8 6.2 33.2 0.9 18.9 1 24.5 1 72.3s-0.2 53.4-1 72.3c-0.8 17.4-3.7 26.9-6.2 33.2 -3.2 8.4-7.1 14.3-13.4 20.6 -6.3 6.3-12.2 10.1-20.6 13.4 -6.3 2.5-15.8 5.4-33.2 6.2 -18.9 0.9-24.5 1-72.3 1s-53.4-0.2-72.3-1c-17.4-0.8-26.9-3.7-33.2-6.2 -8.4-3.2-14.3-7.1-20.6-13.4 -6.3-6.3-10.1-12.2-13.4-20.6 -2.5-6.3-5.4-15.8-6.2-33.2 -0.9-18.9-1-24.5-1-72.3s0.2-53.4 1-72.3c0.8-17.4 3.7-26.9 6.2-33.2 3.2-8.4 7.1-14.3 13.4-20.6 6.3-6.3 12.2-10.1 20.6-13.4 6.3-2.5 15.8-5.4 33.2-6.2C202.6 109.5 208.2 109.3 256 109.3M256 77.1c-48.6 0-54.7 0.2-73.8 1.1 -19 0.9-32.1 3.9-43.4 8.3 -11.8 4.6-21.7 10.7-31.7 20.6 -9.9 9.9-16.1 19.9-20.6 31.7 -4.4 11.4-7.4 24.4-8.3 43.4 -0.9 19.1-1.1 25.2-1.1 73.8 0 48.6 0.2 54.7 1.1 73.8 0.9 19 3.9 32.1 8.3 43.4 4.6 11.8 10.7 21.7 20.6 31.7 9.9 9.9 19.9 16.1 31.7 20.6 11.4 4.4 24.4 7.4 43.4 8.3 19.1 0.9 25.2 1.1 73.8 1.1s54.7-0.2 73.8-1.1c19-0.9 32.1-3.9 43.4-8.3 11.8-4.6 21.7-10.7 31.7-20.6 9.9-9.9 16.1-19.9 20.6-31.7 4.4-11.4 7.4-24.4 8.3-43.4 0.9-19.1 1.1-25.2 1.1-73.8s-0.2-54.7-1.1-73.8c-0.9-19-3.9-32.1-8.3-43.4 -4.6-11.8-10.7-21.7-20.6-31.7 -9.9-9.9-19.9-16.1-31.7-20.6 -11.4-4.4-24.4-7.4-43.4-8.3C310.7 77.3 304.6 77.1 256 77.1L256 77.1z"></path><path d="M256 164.1c-50.7 0-91.9 41.1-91.9 91.9s41.1 91.9 91.9 91.9 91.9-41.1 91.9-91.9S306.7 164.1 256 164.1zM256 315.6c-32.9 0-59.6-26.7-59.6-59.6s26.7-59.6 59.6-59.6 59.6 26.7 59.6 59.6S288.9 315.6 256 315.6z"></path><circle cx="351.5" cy="160.5" r="21.5"></circle></svg></a></li>
+            <li><a href="https://www.facebook.com/rebalancingyou/" title="Visit me on Facebook"><svg viewBox="0 0 512 512" preserveAspectRatio="xMidYMid meet"><path d="M211.9 197.4h-36.7v59.9h36.7V433.1h70.5V256.5h49.2l5.2-59.1h-54.4c0 0 0-22.1 0-33.7 0-13.9 2.8-19.5 16.3-19.5 10.9 0 38.2 0 38.2 0V82.9c0 0-40.2 0-48.8 0 -52.5 0-76.1 23.1-76.1 67.3C211.9 188.8 211.9 197.4 211.9 197.4z"></path></svg></a></li>
           </ul>
+          <button class="header__menu-toggle" v-on:click="toggleMenu">Menu</button>
         </div>
       </div>
       <div class="header__main">
@@ -31,8 +32,8 @@
         <div class="footer__copyright">&copy; Emma Rowland</div>
         <ul class="social social-footer">
           <!--<li><a href="mailto:mail@emmarowland.co.uk" title="Email"><svg viewBox="0 0 512 512" preserveAspectRatio="xMidYMid meet"><path d="M101.3 141.6v228.9h0.3 308.4 0.8V141.6H101.3zM375.7 167.8l-119.7 91.5 -119.6-91.5H375.7zM127.6 194.1l64.1 49.1 -64.1 64.1V194.1zM127.8 344.2l84.9-84.9 43.2 33.1 43-32.9 84.7 84.7L127.8 344.2 127.8 344.2zM384.4 307.8l-64.4-64.4 64.4-49.3V307.8z"></path></svg></a></li>-->
-          <li><a href="https://www.facebook.com/rebalancingyou/" title="Visit me on Facebook"><svg viewBox="0 0 512 512" preserveAspectRatio="xMidYMid meet"><path d="M211.9 197.4h-36.7v59.9h36.7V433.1h70.5V256.5h49.2l5.2-59.1h-54.4c0 0 0-22.1 0-33.7 0-13.9 2.8-19.5 16.3-19.5 10.9 0 38.2 0 38.2 0V82.9c0 0-40.2 0-48.8 0 -52.5 0-76.1 23.1-76.1 67.3C211.9 188.8 211.9 197.4 211.9 197.4z"></path></svg></a></li>
           <li><a href="https://www.instagram.com/rebalancingyou/" title="Visit me on Instagram"><svg viewBox="0 0 512 512" preserveAspectRatio="xMidYMid meet"><path d="M256 109.3c47.8 0 53.4 0.2 72.3 1 17.4 0.8 26.9 3.7 33.2 6.2 8.4 3.2 14.3 7.1 20.6 13.4 6.3 6.3 10.1 12.2 13.4 20.6 2.5 6.3 5.4 15.8 6.2 33.2 0.9 18.9 1 24.5 1 72.3s-0.2 53.4-1 72.3c-0.8 17.4-3.7 26.9-6.2 33.2 -3.2 8.4-7.1 14.3-13.4 20.6 -6.3 6.3-12.2 10.1-20.6 13.4 -6.3 2.5-15.8 5.4-33.2 6.2 -18.9 0.9-24.5 1-72.3 1s-53.4-0.2-72.3-1c-17.4-0.8-26.9-3.7-33.2-6.2 -8.4-3.2-14.3-7.1-20.6-13.4 -6.3-6.3-10.1-12.2-13.4-20.6 -2.5-6.3-5.4-15.8-6.2-33.2 -0.9-18.9-1-24.5-1-72.3s0.2-53.4 1-72.3c0.8-17.4 3.7-26.9 6.2-33.2 3.2-8.4 7.1-14.3 13.4-20.6 6.3-6.3 12.2-10.1 20.6-13.4 6.3-2.5 15.8-5.4 33.2-6.2C202.6 109.5 208.2 109.3 256 109.3M256 77.1c-48.6 0-54.7 0.2-73.8 1.1 -19 0.9-32.1 3.9-43.4 8.3 -11.8 4.6-21.7 10.7-31.7 20.6 -9.9 9.9-16.1 19.9-20.6 31.7 -4.4 11.4-7.4 24.4-8.3 43.4 -0.9 19.1-1.1 25.2-1.1 73.8 0 48.6 0.2 54.7 1.1 73.8 0.9 19 3.9 32.1 8.3 43.4 4.6 11.8 10.7 21.7 20.6 31.7 9.9 9.9 19.9 16.1 31.7 20.6 11.4 4.4 24.4 7.4 43.4 8.3 19.1 0.9 25.2 1.1 73.8 1.1s54.7-0.2 73.8-1.1c19-0.9 32.1-3.9 43.4-8.3 11.8-4.6 21.7-10.7 31.7-20.6 9.9-9.9 16.1-19.9 20.6-31.7 4.4-11.4 7.4-24.4 8.3-43.4 0.9-19.1 1.1-25.2 1.1-73.8s-0.2-54.7-1.1-73.8c-0.9-19-3.9-32.1-8.3-43.4 -4.6-11.8-10.7-21.7-20.6-31.7 -9.9-9.9-19.9-16.1-31.7-20.6 -11.4-4.4-24.4-7.4-43.4-8.3C310.7 77.3 304.6 77.1 256 77.1L256 77.1z"></path><path d="M256 164.1c-50.7 0-91.9 41.1-91.9 91.9s41.1 91.9 91.9 91.9 91.9-41.1 91.9-91.9S306.7 164.1 256 164.1zM256 315.6c-32.9 0-59.6-26.7-59.6-59.6s26.7-59.6 59.6-59.6 59.6 26.7 59.6 59.6S288.9 315.6 256 315.6z"></path><circle cx="351.5" cy="160.5" r="21.5"></circle></svg></a></li>
+          <li><a href="https://www.facebook.com/rebalancingyou/" title="Visit me on Facebook"><svg viewBox="0 0 512 512" preserveAspectRatio="xMidYMid meet"><path d="M211.9 197.4h-36.7v59.9h36.7V433.1h70.5V256.5h49.2l5.2-59.1h-54.4c0 0 0-22.1 0-33.7 0-13.9 2.8-19.5 16.3-19.5 10.9 0 38.2 0 38.2 0V82.9c0 0-40.2 0-48.8 0 -52.5 0-76.1 23.1-76.1 67.3C211.9 188.8 211.9 197.4 211.9 197.4z"></path></svg></a></li>
         </ul>
       </div>
     </footer>
@@ -46,6 +47,18 @@ query {
   }
 }
 </static-query>
+
+<script>
+export default {
+  methods: {
+    toggleMenu () {
+      const bodyClasses = document.querySelector('body').classList
+
+      bodyClasses.contains('menu-active') ? bodyClasses.remove('menu-active') : bodyClasses.add('menu-active')
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 html,
@@ -68,13 +81,44 @@ body {
     background-color: $colour-grey-dark;
 
     & .container {
-      padding: 10px 30px;
-      align-items: flex-end;
+      padding: 0 20px;
+      flex-direction: row;
+      justify-content: space-between;
+
+      @include mq($from: tablet) {
+        padding: 10px 30px;
+        align-items: flex-end;
+      }
     }
   }
 
-  &__social {
-    align-items: flex-end;
+  &__menu-toggle {
+    border: none;
+    font-weight: bold;
+    padding: 10px;
+    outline: 0;
+    font-size: 18px;
+    color: $colour-white;
+    background: none;
+
+    &:after {
+      content: "\2630";
+      width: 20px;
+      display: inline-block;
+      text-align: right;
+    }
+
+    .menu-active &:after {
+      content: "\2715";
+    }
+
+    @include mq($from: tablet) {
+      margin: 34px 10px;
+    }
+
+    @include mq($from: desktop) {
+      display: none;
+    }
   }
 
   &__main {
@@ -96,6 +140,15 @@ body {
     margin: 30px 0 0 0;
     padding: 0;
     border-bottom: 1px solid rgba($colour-white, 0.2);
+    display: none;
+
+    .menu-active & {
+      display: block;
+
+      @include mq($from: tablet) {
+        display: inline-flex;
+      }
+    }
 
     @include mq($from: tablet) {
       margin: 0;
@@ -227,7 +280,11 @@ main {
     }
   }
   &__view-more {
-    font-size: 18px;
+    font-size: .8rem;
+
+    @include mq($from: tablet) {
+      font-size: 18px;
+    }
   }
 }
 
@@ -255,7 +312,7 @@ main {
   margin: 0;
   padding: 0;
   position: relative;
-  left: -2px;
+  left: -10px;
 
   li {
     display: inline-block;
@@ -265,6 +322,7 @@ main {
   a {
     fill: $colour-coral;
     display: block;
+    padding: 7px;
   }
 }
 
@@ -293,6 +351,10 @@ main {
   .social {
     li {
       width: 36px;
+    }
+
+    a {
+      padding: 0;
     }
   }
 }
