59ca4b04cdc18ea31ec48c9109adffe948ab1aae
Andrew Rowland <andrew.rowland@amazerealise.com>
2020-08-11 22:33:41 +0100

    Added download page.

---
+++ src/pages/dowloads.vue
@@ -0,0 +1,25 @@
+<template>
+  <Layout>
+    <banner heading="Downloads" image="banner.png" />
+    <section class="section">
+      <div class="container">
+        <div class="section__container">
+          <h2>Recipe cards</h2>
+          <ul>
+            <li>
+              <a href="/download/recipe-card-fajitas.pdf">Fajitas PDF (550Kb)</a>
+            </li>
+            <li>
+              <a href="/download/recipe-card-mexican-colada.pdf">Mexican Colada PDF (51Kb)</a>
+            </li>
+            <li>
+              <a
+                href="/download/recipe-card-blackbean-and-corn-salsa.pdf"
+              >Blackbean and corn salsa PDF (154Kb)</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  </Layout>
+</template>
