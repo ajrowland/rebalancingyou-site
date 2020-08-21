f0d6066c822025de265132b43a896afa0a5fb283
Andrew Rowland <andrew.rowland@amazerealise.com>
2020-08-21 12:52:54 +0100

    Capitalised.

--- src/pages/downloads/american-diner.vue
+++ src/pages/downloads/american-diner.vue
@@ -5,28 +5,33 @@
       <div class="container">
         <div class="section__container">
           <div>
-            <h2>American diner recipe cards</h2>
+            <h2>American Diner recipe cards</h2>
             <ul>
               <li>
                 <a
                   href="/download/recipe-card-chocolate-brownies.pdf"
                   target="_blank"
-                >Chocolate brownies PDF (227Kb)</a>
+                  >Chocolate brownies PDF (227Kb)</a
+                >
               </li>
               <li>
                 <a
                   href="/download/recipe-card-crunch-coleslaw.pdf"
                   target="_blank"
-                >Crunch coleslaw PDF (755Kb)</a>
+                  >Crunch coleslaw PDF (755Kb)</a
+                >
               </li>
               <li>
-                <a href="/download/recipe-card-hamburger.pdf" target="_blank">Hamburger PDF (466Kb)</a>
+                <a href="/download/recipe-card-hamburger.pdf" target="_blank"
+                  >Hamburger PDF (466Kb)</a
+                >
               </li>
               <li>
                 <a
                   href="/download/recipe-card-spicy-bean-burgers.pdf"
                   target="_blank"
-                >Spicy bean burgers PDF (216Kb)</a>
+                  >Spicy bean burgers PDF (216Kb)</a
+                >
               </li>
             </ul>
           </div>
