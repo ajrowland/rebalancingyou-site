f441b2171c1b0812c45e294a4f8e541689804194
Andrew Rowland <andrew.rowland@amazerealise.com>
2020-08-21 12:37:19 +0100

    Add American diner recipes.

---
+++ src/pages/downloads/american-diner.vue
@@ -0,0 +1,37 @@
+<template>
+  <Layout>
+    <banner heading="Downloads" image="banner.png" />
+    <section class="section">
+      <div class="container">
+        <div class="section__container">
+          <div>
+            <h2>American diner recipe cards</h2>
+            <ul>
+              <li>
+                <a
+                  href="/download/recipe-card-chocolate-brownies.pdf"
+                  target="_blank"
+                >Chocolate brownies PDF (227Kb)</a>
+              </li>
+              <li>
+                <a
+                  href="/download/recipe-card-crunch-coleslaw.pdf"
+                  target="_blank"
+                >Crunch coleslaw PDF (755Kb)</a>
+              </li>
+              <li>
+                <a href="/download/recipe-card-hamburger.pdf" target="_blank">Hamburger PDF (466Kb)</a>
+              </li>
+              <li>
+                <a
+                  href="/download/recipe-card-spicy-bean-burgers.pdf"
+                  target="_blank"
+                >Spicy bean burgers PDF (216Kb)</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  </Layout>
+</template>
